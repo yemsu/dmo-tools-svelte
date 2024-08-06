beda877e2cdd31ef35fb0251b700557ded5a169e
정수진 <sssjsjj@ipartners.co.kr>
2024-08-06 22:34:44 +0900

    fix(style): 탭버튼 컬러 변경

--- src/shared/tabs/ui/Tab.svelte
+++ src/shared/tabs/ui/Tab.svelte
@@ -11,8 +11,8 @@
 		class={cn(
 			'w-[50px] rounded-md py-1 font-bold transition-colors',
 			isActive
-				? 'bg-black/90'
-				: 'opacity-30 hover:bg-black/50 hover:opacity-100',
+				? 'bg-secondary-10'
+				: 'opacity-30 hover:bg-secondary-10/60 hover:opacity-100',
 			className
 		)}
 		on:click
