66372f27606740a3ac4c8e1a594d7242dc1f06d2
정수진 <sssjsjj@ipartners.co.kr>
2024-08-07 19:16:04 +0900

    feat: Master count 1000개인 케이스 추가

--- src/widgets/seal-calculator/config/index.ts
+++ src/widgets/seal-calculator/config/index.ts
@@ -1,12 +1,13 @@
 import type { SealStepsByMaster } from '../types'
 
 export const SEAL_MASTER_COUNT_TYPE = [
-	50, 150, 200, 300, 400, 500, 700, 3000
+	50, 150, 200, 300, 400, 500, 700, 1000, 3000
 ] as const
 export const SEAL_PERCENT_BY_STEPS = [10, 20, 40, 60, 80, 100] as const
 
 export const SEAL_COUNT_STEPS_BY_MASTER: SealStepsByMaster = {
 	3000: [1, 50, 200, 500, 1000, 3000],
+	1000: [null, null, null, null, null, null],
 	700: [1, 30, 150, 300, 500, 700],
 	500: [1, 30, 100, 200, 350, 500],
 	400: [1, 30, null, null, null, 400],
