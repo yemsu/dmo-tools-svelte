0655eec3a6b79b1338882f132c6d81ad0e3e54e0
정수진 <sssjsjj@ipartners.co.kr>
2024-08-13 21:53:35 +0900

    fix: voteCount같을경우 더 빠른시간 우선순위 정렬되도록 수정 #20

--- src/widgets/raid/lib/helper.ts
+++ src/widgets/raid/lib/helper.ts
@@ -1,11 +1,18 @@
 import type { RaidTimeData } from '$entities/raid'
 
-export const timeSortByVote = (times: RaidTimeData[]) =>
-	times.sort((a, b) => b.voteCount - a.voteCount)
+const compareDate = (a: RaidTimeData, b: RaidTimeData) => {
+	const dateA = new Date(a.startAt)
+	const dateB = new Date(b.startAt)
+	return dateA.getTime() - dateB.getTime()
+}
 
 export const timeSortByStartAt = (times: RaidTimeData[]) =>
+	times.sort((a, b) => compareDate(a, b))
+
+export const timeSortByVote = (times: RaidTimeData[]) =>
 	times.sort((a, b) => {
-		const dateA = new Date(a.startAt)
-		const dateB = new Date(b.startAt)
-		return dateA.getTime() - dateB.getTime()
+		if (b.voteCount === a.voteCount) {
+			return compareDate(a, b)
+		}
+		return b.voteCount - a.voteCount
 	})
