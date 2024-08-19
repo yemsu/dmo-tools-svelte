ad561500fbbb4141214186fa40594b1008b75356
정수진 <sssjsjj@gmail.com>
2024-08-19 17:46:44 +0900

    fix: calculator inner 제거

--- src/routes/calculator/+page.svelte
+++ src/routes/calculator/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-	import { Inner } from '$shared/section'
 	import SealCalculator from '$widgets/seal-calculator/ui/SealCalculator.svelte'
 </script>
 
-<Inner class="grid h-content-fill-h gap-2 overflow-hidden py-content-side">
-	<SealCalculator />
-</Inner>
+<SealCalculator />
