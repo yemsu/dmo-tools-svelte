c94298660f6c523c50021d7dc65a55c7b51b380c
정수진 <sssjsjj@gmail.com>
2024-08-23 16:44:36 +0900

    fix: 로그인 추가로 인해 공통 ssr false 설정 제거 #30

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,6 @@ import { TOKEN_NAME, type UserData } from '$entities/user'
 import { decodeJwtResponse } from '$shared/layout/lib/login'
 import type { ServerLoad } from '@sveltejs/kit'
 
-export const prerender = true
 export const load: ServerLoad = async (event) => {
 	const seals = await getSeals()
 	const sealPrices = await getSealPrices('modifiedAt')
