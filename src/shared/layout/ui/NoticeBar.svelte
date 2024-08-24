064aac4759457ba4a583f874de639b66422c1f69
정수진 <sssjsjj@ipartners.co.kr>
2024-08-24 12:58:26 +0900

    fix: notice - param값 생성유무에 반응해서 노출하도록 수정

--- src/shared/layout/ui/NoticeBar.svelte
+++ src/shared/layout/ui/NoticeBar.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
+	import { page } from '$app/stores'
 	import SaveUrlLink from '$shared/layout/ui/SaveUrlLink.svelte'
 	import { onMount } from 'svelte'
 
-	let hasParams = false
+	let showNotice = false
 
 	onMount(() => {
-		hasParams = !!window.location.search
+		showNotice = !!window.location.search
 	})
+
+	const handleShowNotice = () => {
+		showNotice = $page.url.searchParams.size > 0
+	}
+
+	$: $page.url.searchParams && handleShowNotice()
 </script>
 
-{#if hasParams}
+{#if showNotice}
 	<div
-		class="flex-center h-header-h gap-x-2 bg-primary-50 px-content-side text-primary-5 sm:text-xs"
+		class="flex-center h-header-h shrink-0 gap-x-2 bg-primary-50 px-content-side text-primary-5 sm:text-xs"
 	>
 		<p class="text-balance break-keep font-bold">
 			구글 로그인 후 url 업데이트 없이 <br class="md:hidden" /> 편리하게 사이트를
