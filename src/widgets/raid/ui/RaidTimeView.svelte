97f874644990ee6be98ffecacbdd6381c2407b42
정수진 <sssjsjj@ipartners.co.kr>
2024-08-24 11:11:13 +0900

    fix: raid time list 스크롤 없는 현상 수정

--- src/widgets/raid/ui/RaidTimeView.svelte
+++ src/widgets/raid/ui/RaidTimeView.svelte
@@ -11,7 +11,9 @@
 		.map((channel) => +channel) as GameChannel[]
 </script>
 
-<section class="size-full rounded-md py-2 pl-2 backdrop-blur-sm md:pl-4">
+<section
+	class="scroll-box size-full rounded-md py-2 pl-2 backdrop-blur-sm md:pl-4"
+>
 	<div class="mb-4 flex flex-col items-center gap-2 md:flex-row md:gap-3">
 		<Title class="text-center">{raid.name}</Title>
 		<RaidLocation location={raid.location} size="md" />
