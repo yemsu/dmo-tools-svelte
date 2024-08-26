5ab8d7061b2f1b9d1fc59606dd02aa6197f4c8f0
정수진 <sssjsjj@gmail.com>
2024-08-26 09:03:44 +0900

    feat: sitemap xml 생성 설정

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
+import sitemap from 'vite-plugin-sitemap'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -23,6 +24,9 @@ const config = {
 	plugins: [
 		VitePWA({
 			registerType: 'autoUpdate'
+		}),
+		sitemap({
+			hostname: process.env.VITE_BASE_URL
 		})
 	]
 }
