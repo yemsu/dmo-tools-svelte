dd6dc7c00680d0d4c149270fa9364e76dfc89c0f
정수진 <sssjsjj@gmail.com>
2024-08-26 10:45:26 +0900

    fix: 빌드 에러 -  VITE_BASE_URL  호출 에러 수정

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,10 +1,8 @@
-// /src/routes/sitemap.xml/+server.ts
-import { VITE_BASE_URL } from '$env/static/private'
 import type { RequestHandler } from '@sveltejs/kit'
 import * as sitemap from 'super-sitemap'
 
 export const GET: RequestHandler = async () => {
 	return await sitemap.response({
-		origin: VITE_BASE_URL
+		origin: import.meta.env.VITE_BASE_URL
 	})
 }
