3b6e4a853b0b63eeb58be4e84d85ceda7457fdcb
정수진 <sssjsjj@ipartners.co.kr>
2024-09-01 10:43:56 +0900

    chore: 뽑기 에러 공지 제거

--- src/routes/gacha-simulator/+page.svelte
+++ src/routes/gacha-simulator/+page.svelte
@@ -3,19 +3,17 @@
 	import { gachaStore } from '$entities/gacha'
 	import videoGachaBg from '$lib/images/gacha/videos/gacha-bg.mp4'
 	import { META } from '$shared/config'
-	import { Section } from '$shared/section'
 	import {
-		GachaResultView,
-		GachaTitle,
 		GachaCard,
-		GachaResultLoading
+		GachaResultLoading,
+		GachaResultView,
+		GachaTitle
 	} from '$widgets/gacha'
 	import InventoryButton from '$widgets/gacha/ui/inventory/InventoryButton.svelte'
-	import { onDestroy, onMount } from 'svelte'
+	import { onMount } from 'svelte'
 
 	let isLoadingOn = false
 	let isResultVisible = false
-	let isMobile: boolean | null = null
 
 	const endLoadingVideo = () => {
 		isLoadingOn = false
@@ -35,25 +33,13 @@
 		}, 100)
 	}
 
-	const checkMobile = () => {
-		isMobile = window.innerWidth <= 530
-	}
-
 	onMount(() => {
-		checkMobile()
-		window.addEventListener('resize', checkMobile)
-
 		window.addEventListener('beforeunload', (event: BeforeUnloadEvent) => {
 			if ($gachaStore.inventory.length > 0) {
 				event.preventDefault()
 				event.returnValue = '' //for chrome
 			}
 		})
-	})
-
-	onDestroy(() => {
-		if (import.meta.env.SSR) return
-		window.removeEventListener('resize', checkMobile)
 	})
 </script>
 
@@ -62,55 +48,41 @@
 	<meta name="description" content={META.GACHA_SIMULATOR.DESC} />
 </svelte:head>
 
-{#if !isMobile}
-	<section
-		class="bg-gacha relative flex h-full flex-col justify-center font-ns text-gray-200"
-	>
-		<p class="absolute left-0 top-5 w-full text-center text-xs text-[#db5841]">
-			❗ 뽑기 시뮬레이션 확률 오류로 인해 일부 아이템이 나오지 않는 현상이 있어
-			수정하였습니다. (오후 8:10)
-		</p>
-		<div class="relative">
-			<div class="flex-center">
-				<video
-					src={videoGachaBg}
-					autoplay
-					loop
-					muted
-					class="h-[450px] w-[798px] max-w-none"
-				>
-					<track kind="captions" />
-				</video>
-			</div>
-			{#if !isResultVisible}
-				<div
-					class="flex-col-center absolute top-1/2 w-full -translate-y-1/2 gap-10"
-				>
-					<GachaTitle class="max-w-[500px]">
-						소환할 데이터를 선택하세요.
-					</GachaTitle>
-					<div class="flex gap-10">
-						{#each $page.data.gachaList as gachaData (gachaData.id)}
-							<GachaCard {gachaData} on:start={startLoading} />
-						{/each}
-					</div>
-				</div>
-			{/if}
-			<InventoryButton />
+<section
+	class="bg-gacha relative flex h-full flex-col justify-center font-ns text-gray-200"
+>
+	<div class="relative">
+		<div class="flex-center">
+			<video
+				src={videoGachaBg}
+				autoplay
+				loop
+				muted
+				class="h-[450px] w-[798px] max-w-none"
+			>
+				<track kind="captions" />
+			</video>
 		</div>
-		{#if isResultVisible}
-			<GachaResultView
-				on:confirm={onResultViewConfirm}
-				on:start={startLoading}
-			/>
+		{#if !isResultVisible}
+			<div
+				class="flex-col-center absolute top-1/2 w-full -translate-y-1/2 gap-10"
+			>
+				<GachaTitle class="max-w-[500px]">
+					소환할 데이터를 선택하세요.
+				</GachaTitle>
+				<div class="flex gap-10">
+					{#each $page.data.gachaList as gachaData (gachaData.id)}
+						<GachaCard {gachaData} on:start={startLoading} />
+					{/each}
+				</div>
+			</div>
 		{/if}
-		{#if isLoadingOn}
-			<GachaResultLoading on:endVideo={endLoadingVideo} />
-		{/if}
-	</section>
-{:else}
-	<Section size="sm" class="text-center">
-		<p class="text-[50px]">🏄🏿‍♂️</p>
-		<p>모바일은 작업 중 이예요. <br />조금만 기다려주세요!</p>
-	</Section>
-{/if}
+		<InventoryButton />
+	</div>
+	{#if isResultVisible}
+		<GachaResultView on:confirm={onResultViewConfirm} on:start={startLoading} />
+	{/if}
+	{#if isLoadingOn}
+		<GachaResultLoading on:endVideo={endLoadingVideo} />
+	{/if}
+</section>
