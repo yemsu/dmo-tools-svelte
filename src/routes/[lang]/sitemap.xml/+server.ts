0b058c3bb872b13344202bed6010b34915588084
정수진 <sssjsjj@gmail.com>
2024-10-14 16:45:15 +0900

    fix: sitemap 설정 수정

--- src/routes/[lang]/sitemap.xml/+server.ts
+++ src/routes/[lang]/sitemap.xml/+server.ts
@@ -1,15 +1,16 @@
 import { PATH } from '$shared/config'
+import { _objKeys } from '$shared/lib'
 import type { RequestHandler } from '@sveltejs/kit'
 import * as sitemap from 'super-sitemap'
 
 export const GET: RequestHandler = async () => {
 	return await sitemap.response({
 		origin: import.meta.env.VITE_BASE_URL,
-		excludeRoutePatterns: [
-			PATH.JOIN,
-			PATH.LOGIN,
-			PATH.SAVE_URL,
-			PATH.PRIVACY_POLICY
-		]
+		excludeRoutePatterns: _objKeys(PATH)
+			.map((page) => {
+				const restPath = PATH[page] === '/' ? '' : PATH[page]
+				return [`/kr${restPath}`, `/en${restPath}`]
+			})
+			.flat()
 	})
 }
