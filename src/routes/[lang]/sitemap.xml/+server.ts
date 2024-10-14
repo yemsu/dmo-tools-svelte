dc65b0849f956096ff69aab559c9352def657f69
정수진 <sssjsjj@gmail.com>
2024-10-14 16:51:01 +0900

    fix: 영문 버전에서는 노출하지 않는 페이지 sitemap에서 제외

--- src/routes/[lang]/sitemap.xml/+server.ts
+++ src/routes/[lang]/sitemap.xml/+server.ts
@@ -3,13 +3,19 @@ import { _objKeys } from '$shared/lib'
 import type { RequestHandler } from '@sveltejs/kit'
 import * as sitemap from 'super-sitemap'
 
+const onlyKrPaths = ['raid-timer', 'save-url']
+
 export const GET: RequestHandler = async () => {
 	return await sitemap.response({
 		origin: import.meta.env.VITE_BASE_URL,
 		excludeRoutePatterns: _objKeys(PATH)
 			.map((page) => {
 				const restPath = PATH[page] === '/' ? '' : PATH[page]
-				return [`/kr${restPath}`, `/en${restPath}`]
+				const result = [`/kr${restPath}`, `/en${restPath}`]
+				if (onlyKrPaths.find((onlyKrPath) => restPath.includes(onlyKrPath))) {
+					return result[0]
+				}
+				return result
 			})
 			.flat()
 	})
