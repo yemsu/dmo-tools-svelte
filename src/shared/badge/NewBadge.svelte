e002e6dfa4386a2c4cab30b847f36e2e5277ac50
정수진 <sssjsjj@gmail.com>
2024-10-28 09:30:11 +0900

    chore: 뽑기 시뮬 new 뱃지 제거

--- src/shared/badge/NewBadge.svelte
+++ src/shared/badge/NewBadge.svelte
@@ -3,13 +3,13 @@
 	const { class: className } = $$restProps
 </script>
 
-<span
+<!-- <span
 	class={cn(
 		'flex-center absolute right-0 top-0 -translate-y-[40%] translate-x-[120%]',
 		'aspect-square w-[1.5em]',
-		'bg-red rounded-full text-[6px] text-gray-200 md:text-[8px]',
+		'rounded-full bg-red text-[6px] text-gray-200 md:text-[8px]',
 		className
 	)}
 >
 	N
-</span>
+</span> -->
