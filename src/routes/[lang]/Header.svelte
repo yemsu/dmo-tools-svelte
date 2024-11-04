92db7ed8e11a885fc905930e5f7185a4ead922ea
정수진 <sssjsjj@gmail.com>
2024-11-04 17:02:00 +0900

    refactor: calculator 관련 데이터 호출 f로직 페이지 layout 쪽으로 이동

--- src/routes/[lang]/Header.svelte
+++ src/routes/[lang]/Header.svelte
@@ -1,75 +1,11 @@
 <script lang="ts">
 	import { page } from '$app/stores'
-	import {
-		mySealCounts,
-		mySealPrices,
-		myStats,
-		type Stats,
-		STATS,
-		STATS_PERCENT_TYPE,
-		type StatType
-	} from '$entities/seals'
-	import { user } from '$entities/user'
 	import { AuthButton } from '$shared/layout'
 	import LangButton from '$shared/layout/ui/LangButton.svelte'
-	import { objectBy } from '$shared/lib'
 	import { Inner } from '$shared/section'
 	import type { LangType } from '$shared/types'
-	import { getMySealData } from '$widgets/my-seals'
-	import { getCurrentStep } from '$widgets/seal-calculator'
 
 	$: lang = $page.data.lang as LangType
-	$: statCalc = (statType: StatType) => {
-		if ($mySealCounts.length === 0) return
-		const mySealsByStatType = objectBy(
-			$mySealCounts,
-			({ id }) => getMySealData($page.data.seals, id).statType
-		)
-		if (!mySealsByStatType) return 0
-		const sealsByStatType = mySealsByStatType[statType]
-		if (!sealsByStatType || sealsByStatType.length === 0) {
-			return 0
-		}
-		let resultValue = 0
-		sealsByStatType.forEach(({ id, count }) => {
-			let sealPercent = 0
-			const seal = getMySealData($page.data.seals, id)
-			const crrStat = getCurrentStep(seal, count)
-			sealPercent = crrStat.percent
-			const maxIncrease = seal.maxIncrease
-			resultValue += maxIncrease * (sealPercent / 100)
-		})
-		if (STATS_PERCENT_TYPE.includes(statType)) {
-			resultValue = resultValue / 100
-		}
-		return resultValue
-	}
-	// my seals
-	const setMyData = async () => {
-		await mySealCounts.load()
-		await mySealPrices.load()
-	}
-	$: $user && setMyData()
-
-	const resetMyData = () => {
-		mySealCounts.reset()
-		mySealPrices.reset()
-	}
-	$: !$user && resetMyData()
-
-	// my stats
-	const setMyStats = () => {
-		if ($mySealCounts.length === 0) return
-		const newStats = STATS.reduce((result, { type }) => {
-			const statTypeCalc = statCalc(type)
-			if (statTypeCalc === undefined) return result
-			result[type] = statTypeCalc
-			return result
-		}, {} as Stats)
-		myStats.set(newStats)
-	}
-
-	$: $mySealCounts && setMyStats()
 </script>
 
 <header>
--- src/routes/[lang]/calculator/+layout.svelte
+++ src/routes/[lang]/calculator/+layout.svelte
@@ -1,14 +1,51 @@
 <script lang="ts">
-	import type { LangType } from '$shared/types'
-	import { mySealCounts } from '$entities/seals'
 	import { page } from '$app/stores'
+	import { MENUS } from '$entities/menus'
+	import {
+		mySealCounts,
+		mySealPrices,
+		myStats,
+		STATS,
+		STATS_PERCENT_TYPE,
+		type Stats,
+		type StatType
+	} from '$entities/seals'
+	import { user } from '$entities/user'
+	import { PATH } from '$shared/config'
+	import { objectBy } from '$shared/lib'
+	import { Section } from '$shared/section'
 	import Tab from '$shared/tabs/ui/Tab.svelte'
 	import Tabs from '$shared/tabs/ui/Tabs.svelte'
-	import { PATH } from '$shared/config'
-	import { Section } from '$shared/section'
-	import { MENUS } from '$entities/menus'
+	import type { LangType } from '$shared/types'
+	import { getMySealData } from '$widgets/my-seals'
+	import { getCurrentStep } from '$widgets/seal-calculator'
 
 	$: lang = $page.data.lang as LangType
+	$: statCalc = (statType: StatType) => {
+		if ($mySealCounts.length === 0) return
+		const mySealsByStatType = objectBy(
+			$mySealCounts,
+			({ id }) => getMySealData($page.data.seals, id).statType
+		)
+		if (!mySealsByStatType) return 0
+		const sealsByStatType = mySealsByStatType[statType]
+		if (!sealsByStatType || sealsByStatType.length === 0) {
+			return 0
+		}
+		let resultValue = 0
+		sealsByStatType.forEach(({ id, count }) => {
+			let sealPercent = 0
+			const seal = getMySealData($page.data.seals, id)
+			const crrStat = getCurrentStep(seal, count)
+			sealPercent = crrStat.percent
+			const maxIncrease = seal.maxIncrease
+			resultValue += maxIncrease * (sealPercent / 100)
+		})
+		if (STATS_PERCENT_TYPE.includes(statType)) {
+			resultValue = resultValue / 100
+		}
+		return resultValue
+	}
 
 	$: SUB_MENUS = [
 		{
@@ -33,6 +70,32 @@
 			path: PATH.MY_SEALS
 		}
 	]
+	// my seals
+	const setMyData = async () => {
+		await mySealCounts.load()
+		await mySealPrices.load()
+	}
+	$: $user && setMyData()
+
+	const resetMyData = () => {
+		mySealCounts.reset()
+		mySealPrices.reset()
+	}
+	$: !$user && resetMyData()
+
+	// my stats
+	const setMyStats = () => {
+		if ($mySealCounts.length === 0) return
+		const newStats = STATS.reduce((result, { type }) => {
+			const statTypeCalc = statCalc(type)
+			if (statTypeCalc === undefined) return result
+			result[type] = statTypeCalc
+			return result
+		}, {} as Stats)
+		myStats.set(newStats)
+	}
+
+	$: $mySealCounts && setMyStats()
 </script>
 
 <Section>
