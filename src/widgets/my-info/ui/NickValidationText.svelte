b8937a692f94654f69a94b5b836aea002f1a1b69
정수진 <sssjsjj@gmail.com>
2024-11-05 15:37:46 +0900

    fix: 닉네임 validationText 영문 텍스트 추가

--- src/widgets/my-info/ui/NickValidationText.svelte
+++ src/widgets/my-info/ui/NickValidationText.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
+	import type { LangType } from '$shared/types'
+	import { _ } from 'svelte-i18n'
 	import { getNickCheck } from '$entities/user'
+	import { page } from '$app/stores'
 
 	export let value: string | null
 	export let setIsValid: (isValue: boolean) => void
 	let prevValue = value
+	$: lang = $page.data.lang as LangType
+
 	const VALIDATIONS = {
-		noValue: '닉네임을 입력해 주세요.',
-		specialCharacter: '특수문자/공백은 사용할 수 없어요.',
-		min: '2자 이상 작성해 주세요.',
-		max: '최대 10자까지 작성할 수 있어요.',
-		duplicated: '이미 사용중인 닉네임 이예요.'
+		noValue: {
+			kr: '닉네임을 입력해 주세요.',
+			en: 'Please enter a nickname.'
+		},
+		specialCharacter: {
+			kr: '특수문자/공백은 사용할 수 없어요.',
+			en: 'Special characters/spaces are not allowed.'
+		},
+		min: {
+			kr: '2자 이상 작성해 주세요.',
+			en: 'Please use at least 2 characters.  '
+		},
+		max: {
+			kr: '최대 10자까지 작성할 수 있어요.',
+			en: 'You can use up to a maximum of 10 characters.'
+		},
+		duplicated: {
+			kr: '이미 사용중인 닉네임 이예요.',
+			en: 'This nickname is already in use.'
+		}
 	} as const
 
 	type ValidationTypes = keyof typeof VALIDATIONS
@@ -60,9 +80,9 @@
 <div>
 	{#if inValidTypes.length > 0}
 		{#each inValidTypes as inValidType (inValidType)}
-			<p class="text-xs text-warning">{VALIDATIONS[inValidType]}</p>
+			<p class="text-xs text-warning">{VALIDATIONS[inValidType][lang]}</p>
 		{/each}
 	{:else if value !== null}
-		<p class="text-xs text-point">사용 가능한 닉네임 입니다.</p>
+		<p class="text-xs text-point">{$_('available_nickname')}</p>
 	{/if}
 </div>
