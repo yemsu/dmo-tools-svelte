30bd58328365c387973b6517ca8395f104ae1a1a
정수진 <sssjsjj@gmail.com>
2024-11-13 13:57:14 +0900

    fix: prerender 설정으로 인해 redirect hooks실행되지 않아 vercel.json으로 리다이렉트 처리

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,19 +2,6 @@
 import type { Handle } from '@sveltejs/kit'
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// 구버전 도메인으로 접속시 현재 도메인으로 redirect
-	if (!event.isDataRequest && !import.meta.env.DEV) {
-		const host = event.request.headers.get('host')
-		if (host?.includes('dmo-tools.vercel.app')) {
-			return new Response(null, {
-				status: 301,
-				headers: {
-					location: `${import.meta.env.VITE_BASE_URL}${event.url.pathname}${event.url.search}`
-				}
-			})
-		}
-	}
-
 	// html lang 설정
 	const lang = event.url.pathname.split('/')[1]
 	const htmlLang = ['kr', 'en'].includes(lang)
