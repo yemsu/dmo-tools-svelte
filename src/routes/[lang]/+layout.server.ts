995b60b2ea822d02e11078b8eba0337df72b59e1
정수진 <sssjsjj@gmail.com>
2024-11-13 14:15:07 +0900

    fix: prerendering 되지 않아 하위 layout에도 추가

--- src/routes/[lang]/+layout.server.ts
+++ src/routes/[lang]/+layout.server.ts
@@ -1,6 +1,8 @@
 import type { ServerLoad } from '@sveltejs/kit'
 import { locale, waitLocale } from 'svelte-i18n'
 
+export const prerender = true
+
 export const load: ServerLoad = async ({ url }) => {
 	const path = url.pathname
 	const lang = path.startsWith('/en') ? 'en' : 'kr'
