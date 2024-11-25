6a839c0ce84e665ab113399cee98758b0de276f5
정수진 <sssjsjj@gmail.com>
2024-11-25 10:00:55 +0900

    fix: 씰 게싼 결과 레이처 위치, 사이즈 수정

--- src/widgets/seal-calculator/ui/CalcResult.svelte
+++ src/widgets/seal-calculator/ui/CalcResult.svelte
@@ -30,41 +30,48 @@
 	$: resultUnit = isPercentType ? '%' : ''
 </script>
 
-<section
+<div
 	class={cn(
-		'fixed z-toast overflow-hidden text-center',
-		'h-[var(--result-h)] w-[var(--result-w)]',
-		'bottom-[var(--result-b)] left-[var(--result-l)]',
-		'port:flex-col land:rounded-full',
-		'rounded-md border-2 border-gray-10 bg-deep drop-shadow-lg'
+		'flex-center fixed z-toast text-center',
+		'left-[var(--result-l)]',
+		'bottom-[var(--result-b)]',
+		'w-[var(--result-area-w)]'
 	)}
 >
-	<h2 class="ir">
-		<TextByLang
-			text="계산 결과 - 총 능력치, 총 비용"
-			engText="Calculation Results - Total Stats, Total Cost "
-		/>
-	</h2>
-	<div class="flex flex-wrap">
-		<div class="flex-center flex-1 p-2 land:p-4">
-			<p class="flex-center gap-2 text-body-md leading-none">
-				{#each values as value (value.title.kr)}
-					<span class="flex flex-col gap-[2px] whitespace-nowrap land:gap-1">
-						<span class="text-sub-lg text-gray-11 land:text-sub-md">
-							{value.title[$lang]}
+	<section
+		class={cn(
+			'h-calc-result-h w-[var(--result-w)] overflow-hidden',
+			'port:flex-col land:rounded-full',
+			'rounded-md border-2 border-gray-10 bg-deep drop-shadow-lg'
+		)}
+	>
+		<h2 class="ir">
+			<TextByLang
+				text="계산 결과 - 총 능력치, 총 비용"
+				engText="Calculation Results - Total Stats, Total Cost "
+			/>
+		</h2>
+		<div class="flex flex-wrap">
+			<div class="flex-center flex-1 p-2 land:p-4">
+				<p class="flex-center gap-2 text-body-md leading-none">
+					{#each values as value (value.title.kr)}
+						<span class="flex flex-col gap-[2px] whitespace-nowrap land:gap-1">
+							<span class="text-sub-lg text-gray-11 land:text-sub-md">
+								{value.title[$lang]}
+							</span>
+							<span class="text-sub-md font-bold text-point land:text-body-lg">
+								{value.value}{resultUnit}
+							</span>
 						</span>
-						<span class="text-sub-md font-bold text-point land:text-body-lg">
-							{value.value}{resultUnit}
-						</span>
-					</span>
-					{#if value.afterMark}
-						<span>{value.afterMark}</span>
-					{/if}
-				{/each}
+						{#if value.afterMark}
+							<span>{value.afterMark}</span>
+						{/if}
+					{/each}
+				</p>
+			</div>
+			<p class="flex-center w-full bg-background p-1 land:w-[40%] land:p-4">
+				<StatBarTotalPrice totalPrice={willNeedMoneyTotal} />
 			</p>
 		</div>
-		<p class="flex-center w-full bg-background p-1 land:w-[40%] land:p-4">
-			<StatBarTotalPrice totalPrice={willNeedMoneyTotal} />
-		</p>
-	</div>
-</section>
+	</section>
+</div>
