27a43e2e5134d1605c3393f9cd0d1a0d9c9cd940
정수진 <sssjsjj@gmail.com>
2025-01-08 15:59:30 +0900

    chore: 시즌 아이콘 제거

--- src/widgets/header/HeaderLogo.svelte
+++ src/widgets/header/HeaderLogo.svelte
@@ -15,6 +15,7 @@
 		class="block land:px-2"
 		title={$lang === 'kr' ? '메인으로' : 'Go to Main Page'}
 	>
-		DMO tools<span class="text-[0.75em]">🎄</span>
+		DMO tools
+		<!-- <span class="text-[0.75em]">🎄</span> -->
 	</a>
 </h1>
