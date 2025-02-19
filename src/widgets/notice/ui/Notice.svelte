85d5d1fb909160aff95d4ee4195a47d029be2973
정수진 <sssjsjj@ipartners.co.kr>
2025-02-19 20:05:18 +0900

    chore: 뽑기 시뮬 추가 관련 공지

--- src/widgets/notice/ui/Notice.svelte
+++ src/widgets/notice/ui/Notice.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { lang } from '$shared/model'
 	import { Title } from '$shared/text'
-	import Notice1107 from '$widgets/notice/ui/Notice1107.svelte'
 	import Notice1112 from '$widgets/notice/ui/Notice1112.svelte'
 	import Notice1128 from '$widgets/notice/ui/Notice1128.svelte'
 	import Notice1217 from '$widgets/notice/ui/Notice1217.svelte'
+	import Notice250219 from '$widgets/notice/ui/Notice250219.svelte'
 	import { _ } from 'svelte-i18n'
 
 	$: isKr = $lang === 'kr'
@@ -18,6 +18,7 @@
 	>
 	<div class="flex-center flex-1 px-2 py-1">
 		<div>
+			<Notice250219 />
 			<Notice1217 />
 			{#if isKr}
 				<Notice1128 />
---
+++ src/widgets/notice/ui/Notice250219.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { lang } from '$shared/model'
+	import NoticeItem from './NoticeItem.svelte'
+</script>
+
+<NoticeItem
+	date="2025.02.19"
+	startDate="Wed Feb 19 2025 19:48:05 GMT+0900"
+	modalSize="lg"
+	title={{
+		kr: '블룸로드몬 데이터 소환이 추가되었습니다.',
+		en: 'The Seal Efficiency Calculator logic has been improved.'
+	}}
+>
+	{#if $lang === 'kr'}
+		<p>
+			안녕하세요, 오랜만에 인사드립니다. 모두 잘 지내고 계신가요? <br />
+			이번에 블룸로드몬 데이터 소환이 새롭게 추가되어 시뮬레이터에 업데이트 하였습니다.
+			<br />
+			시뮬레이터로 제물 바치시고 뽑기 하시는 분들 모두 천장 전에 뽑으시길 기원합니다.
+			🎊🎊🎊
+			<br />
+			오류 제보나 개선 의견은 언제나 환영합니다. 즐거운 디마되세요!<br />
+		</p>
+	{:else}
+		<p>
+			Hello everyone! It's been a while. How have you all been? <br />
+			We've updated the simulator with newly added Bloomlordmon summoning data.
+			<br />
+			For those of you who make offerings and pull cards in the simulator, I hope
+			you get your desired cards before hitting pity! 🎊 <br />
+			Bug reports and suggestions for improvement are always welcome!
+		</p>
+	{/if}
+</NoticeItem>
